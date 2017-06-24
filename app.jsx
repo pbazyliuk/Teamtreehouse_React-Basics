94445e560ef2273569a97124e00a289a86b6f93b
Pavlo <p.bazyliuk@gmail.com>
2017-06-24 21:33:51 +0300

    added stopwatch timer component

--- app.jsx
+++ app.jsx
@@ -27,6 +27,72 @@ var PLAYERS = [
     }
 ];
 
+class StopWatch extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            running: false,
+            elapsedTime: 0,
+            previousTime: 0,
+        };
+
+        this.onStart = this.onStart.bind(this);
+        this.onStop = this.onStop.bind(this);
+        this.onReset = this.onReset.bind(this);
+        this.onTick = this.onTick.bind(this);
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(this.onTick, 100);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    onTick() {
+        if(this.state.running) {
+            var now = Date.now();
+            this.setState({
+                previousTime: now,
+                elapsedTime: this.state.elapsedTime + ( now - this.state.previousTime),
+            })
+        }
+        console.log('onclick');
+    }
+
+
+
+    onStart() {
+        this.setState({ running: true, previousTime: Date.now()});
+    }
+
+    onStop() {
+        this.setState({ running: false});
+    }
+    
+    onReset() {
+        this.setState({ elapsedTime: 0, previousTime: Date.now() });
+    }
+
+    render() {
+        var seconds = Math.floor(this.state.elapsedTime / 1000);
+        return (
+            <div className="stopwatch">
+                <h2>Stopwatch</h2>
+                <div className="stopwatch-time">{seconds}</div>
+                {this.state.running ?
+                    <button onClick={this.onStop}>Stop</button> 
+                    :
+                    <button onClick={this.onStart}>Start</button>
+                }
+                <button onClick={this.onReset}>Reset</button>
+            </div>
+        )
+    }
+}
+
+
 class AddPlayerForm extends React.Component {
     constructor(props) {
         super(props);
@@ -103,6 +169,7 @@ function Header(props) {
         <div className="header">
             <Stats players={props.players}/>
             <h1>{props.title}</h1>
+            <StopWatch />
         </div>
     );
 }
